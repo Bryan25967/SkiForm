51f1eba3f4cbf05865f5a5a8c4f883730c984f66
Bryan DeFabio <Bryandefabio@gmail.com>
2022-04-06 15:40:48 -0700

    major firebase fixes and deployment

--- app.js
+++ app.js
@@ -18,20 +18,25 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCI-n_JcRwS-Y7MTkcygd96q9W-GxAOxrc",
-    authDomain: "ski-form.firebaseapp.com",
-    databaseURL: "https://ski-form-default-rtdb.firebaseio.com",
-    projectId: "ski-form",
-    storageBucket: "ski-form.appspot.com",
-    messagingSenderId: "785907061572",
-    appId: "1:785907061572:web:4111878ac5805be958bd11",
-    measurementId: "G-3MSMB6QMXT"
-  };
-  
-  // email change
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCtxY9curZWGN86h3sP0kldCAoeBJcfJ1A",
+  authDomain: "ski-forum-1c479.firebaseapp.com",
+  projectId: "ski-forum-1c479",
+  storageBucket: "ski-forum-1c479.appspot.com",
+  messagingSenderId: "776595877175",
+  appId: "1:776595877175:web:283ae5ff1612659a607eb2",
+  measurementId: "G-RT1JP5X1KJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
   const mySkiDatatbase = getDatabase(app);
 
 const auth = getAuth ();
